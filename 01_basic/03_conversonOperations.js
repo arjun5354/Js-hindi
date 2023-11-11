4765cc54548e138aa8ece4180a1b9dd6c5415973
Arjun Nayak <140893255+arjun5354@users.noreply.github.com>
2023-11-11 13:52:03 +0000

    operations

--- 01_basic/03_conversonOperations.js
+++ 01_basic/03_conversonOperations.js
@@ -25,3 +25,35 @@ let somenumber=33
 let stringsomenumber=String(somenumber)
 console.log(typeof stringsomenumber );
 console.log(stringsomenumber );
+
+
+// ********************** Operations ************************
+
+let value=3
+let negvalue=-value;
+console.log(negvalue);
+
+console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2/3);
+console.log(2**3);
+console.log(2%3);
+
+let str1="hello"
+let str2= " Arjun"
+
+console.log(str1+str2);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(2+1+"2");
+console.log(3+4*5%3);
+
+console.log(+true)
+console.log(+"")
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
