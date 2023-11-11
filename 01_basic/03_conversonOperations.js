b181e701e4f9625e6dd4ede81164f66eaec42241
Arjun Nayak <140893255+arjun5354@users.noreply.github.com>
2023-11-11 13:21:32 +0000

    conversionofdatatypes

---
+++ 01_basic/03_conversonOperations.js
@@ -0,0 +1,27 @@
+// let score="33abc"
+// let score=null
+// let score=undefined
+let score=true
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber));
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// Note : 
+// "33" => 33
+// "33abd"=>NaN
+
+let isloggedIn=1;
+let booleanisloggedIn=Boolean(isloggedIn)
+console.log(typeof booleanisloggedIn);
+console.log( booleanisloggedIn);
+// 1->true, 0->false
+// ""->false,"Arjun"->true
+
+let somenumber=33
+let stringsomenumber=String(somenumber)
+console.log(typeof stringsomenumber );
+console.log(stringsomenumber );
