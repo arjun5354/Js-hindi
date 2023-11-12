8549db58073e8beff8841ec16b954f03cddd876e
Arjun Nayak <140893255+arjun5354@users.noreply.github.com>
2023-11-12 18:54:25 +0000

    datatypesummery

---
+++ 01_basic/datatypes-summery.js
@@ -0,0 +1,30 @@
+// primitive // call by value
+
+// 7 types : String , Number,Bollean,null,undefined,Symbol,BigInt
+
+// Refrence Type/ Non premitive
+
+// Array,objects,Functions
+
+// Symbol->Stores unique value
+const id=Symbol('123')
+const Anotherid=Symbol('123')
+console.log(id==Anotherid);
+
+// BigInt
+const bigNumber=34235246563454232352354633463n;
+console.log(bigNumber);
+console.log(typeof bigNumber);
+// Array, Objects,Function
+
+const heros=[ "Shaktiman","naagraj","doga"]
+
+let obj={
+    name:"Arjun",
+    age:22,
+}
+
+const myfunction=function () {
+    console.log("Hello World");
+}
+myfunction();
